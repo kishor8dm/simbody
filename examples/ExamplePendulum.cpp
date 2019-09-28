341e1ef717b3e4e1ce5833629bd9ed3f2f74ccd1
kishor8dm <kishor8dm@gmail.com>
2019-09-27 23:15:17 -0700

    Ranndom orientation

--- examples/ExamplePendulum.cpp
+++ examples/ExamplePendulum.cpp
@@ -21,15 +21,15 @@
  * limitations under the License.                                             *
  * -------------------------------------------------------------------------- */
 
-
-// Define two identical double pendulums, one modeled the easy way using
-// two pin mobilizers, the other modeled with free mobilizers plus a ball
-// constraint, plus two "constant angle" constraints to get rid of the extra 
-// rotational degrees of freedom.
-// We're going to show that the resulting reaction forces are identical.
-
-
 #include "Simbody.h"
+
+namespace {
+//-0.1504133023, -0.7202261496  0.6643972918  0.1312492688
+//-0.8802997333,  -0.4219333635  -0.1958447339 -0.09321725385
+//0.1707965794, -0.1900937466 -0.1176534344 -0.9596095901
+//0.08860797748, 0.4014763757 0.3004798435 0.8606260569
+//0.3201724846,  0.7169894904  0.3068382102 -0.5378345131
+}
 
 using namespace SimTK;
 using std::cout; using std::endl;
@@ -40,6 +40,11 @@ int main() {
     MultibodySystem system;
     SimbodyMatterSubsystem matter(system);
     GeneralForceSubsystem forces(system);
+    Quaternion q0(-0.1504133023, -0.7202261496,  0.6643972918,  0.1312492688);
+    Quaternion q1(-0.8802997333,  -0.4219333635,  -0.1958447339, -0.09321725385);
+    Quaternion q2(0.1707965794, -0.1900937466, -0.1176534344, -0.9596095901);
+    Quaternion q3(0.3201724846,  0.7169894904,  0.3068382102, -0.5378345131);
+
 
     // Add gravity as a force element.
     Rotation x45(Pi/4, XAxis);
@@ -49,24 +54,24 @@ int main() {
     Body::Rigid pendulumBody(MassProperties(1.0, Vec3(0), Inertia(1)));
 
     MobilizedBody::Pin pendulum0(matter.updGround(),
-                                Transform(/*x45,*/Vec3(0,-1,0)), 
+                                Transform(Rotation(q0), Vec3(0,-1,0)),
                                 pendulumBody, 
-                                Transform(Vec3(0, 1, 0)));
+                                Transform(Rotation(q1),Vec3(0, 1, 0)));
     MobilizedBody::Pin pendulum1(pendulum0,
-                                Transform(/*x45,*/Vec3(0,-1,0)), 
+                                Transform(Rotation(q2), Vec3(0,-1,0)),
                                 pendulumBody, 
-                                Transform(Vec3(0, 1, 0)));
+                                Transform(Rotation(q3), Vec3(0, 1, 0)));
     system.realizeTopology();
     State state = system.getDefaultState();
     pendulum0.setOneQ(state, 0, Pi/4);
     pendulum1.setOneQ(state, 0, Pi/4);
 
-    system.realize(state);
+    pendulum0.setOneU(state, 0, Pi/4);
+    pendulum1.setOneU(state, 0, Pi/4);
 
+    system.realize(state);
     std::cout << pendulum0.getBodyRotation(state).convertRotationToQuaternion() << "\n";
     std::cout << pendulum1.getBodyRotation(state).convertRotationToQuaternion() << "\n";
-    std::cout << pendulum0.getBodyMassCenterStation(state) << std::endl;
-    std::cout << pendulum1.getBodyMassCenterStation(state) << std::endl;
   } catch (const std::exception& e) {
       std::cout << "ERROR: " << e.what() << std::endl;
       return 1;
